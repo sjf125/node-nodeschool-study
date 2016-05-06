5e983e8df3ab94fc5af77b1156326967eb5362ea
sjf125 <sjf125@gmail.com>
2016-05-06 08:57:56 -0400

    Complete exercise 5

--- learnyounode/ex3.js
+++ learnyounode/ex3.js
@@ -5,5 +5,3 @@ let fs = require('fs')
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   console.log(data.split('\n').length - 1);
 });
-
-// cb(process.argv[2], readFile(process.argv[2]), )
---
+++ learnyounode/ex4.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+
+var mydir = process.argv[2];
+var ext1 = '.' + process.argv[3];
+
+fs.readdir(mydir, function(err, files){
+  if(err){
+    throw err
+  }
+  files.forEach(function(filename){
+    var ext = path.extname(filename);
+    if(ext == ext1){
+      console.log(filename);
+    }
+  });
+});
